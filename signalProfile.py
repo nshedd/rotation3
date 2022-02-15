1967006743515d70c0677f2cdcc4937a22fd7823
nshedd <58157630+nshedd@users.noreply.github.com>
2022-02-15 09:24:20 -0500

    transpose files and loci

--- signalProfile.py
+++ signalProfile.py
@@ -1,3 +1,5 @@
+## run as: python3 signalProfile.py /data/rusers/sheddn/TR-ATACseq/all_files.txt /data/zusers/fankaili/ccre/ubi-rDHS_V3/GRCh38_ubi-rDHS_V3.txt /data/rusers/sheddn/ubi_rDHS/GRCh38_ubi-rDHS_V3_signals.tsv
+
 ## inputs:
 ##      BW_files_list: list of /path/to/file.bigwig
 ##      loci_list: list of chr1 10000000    10001000
@@ -19,27 +21,31 @@ with open(BW_files_list, 'r') as file:
         BW_files.extend(line.split()) 
 
 loci_list = pd.read_csv(loci_file, header=None, sep='\t')
+print(loci_list)
 
+loci_table = loci_list[0] + ":" + loci_list[1].astype(str) + "-" + loci_list[2].astype(str)
+loci_table = loci_table.tolist()
+print(loci_table)
 
-dataframe = pd.DataFrame(data={'File': BW_files})
+dataframe = pd.DataFrame(data={'Locus': loci_table})
 
-for locus in (range(0,len(loci_list))):
-    print(locus)
-    chrom = loci_list.iat[locus,0].split()[0]
-    loc1 = int(loci_list.iat[locus,0].split()[1])
-    loc2 = int(loci_list.iat[locus,0].split()[2])
-    print(loc1)
-    print(loc2)
-
-    signal_values = []
-
-    for file in BW_files:
+for file in BW_files:
         print(file)
-        with pyBigWig.open(file) as b:
-            values = [ x if not np.isnan(x) else 0 for x in b.values(chrom, loc1, loc2) ]
-        mean_signal = sum(values) / float(len(values))
-        signal_values.append(mean_signal)
 
-    dataframe[locus] = signal_values
+        signal_values = []
+
+        for locus in (range(0,len(loci_list))):
+            chrom = loci_list.iat[locus,0]
+            loc1 = int(loci_list.iat[locus,1])
+            loc2 = int(loci_list.iat[locus,2])
+
+            with pyBigWig.open(file) as b:
+                values = [ x if not np.isnan(x) else 0 for x in b.values(chrom, loc1, loc2) ]
+            mean_signal = sum(values) / float(len(values))
+            signal_values.append(mean_signal)
+
+        dataframe[file] = signal_values
+
 dataframe.to_csv(output_name, sep='\t', index=False)
+
 
